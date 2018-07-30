d5e48b0a4bad4cd0748d414566e2f5d5808d3ea0
HeTrickPH <35405843+HeTrickPH@users.noreply.github.com>
2018-07-30 09:36:49 -0500

    Home&about page content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,45 +5,28 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <div className="Home_el-1">
-          <h1 className="somecoolheader">SOME COOL HEADER</h1>
+          <h1 className="somecoolheader">A New Roof is NOT Something to Gamble With</h1>
           <article>
             {" "}
-            some cool content goes here blah blah The Center for Information
-            Industry Development (CCID), under China’s Ministry of Industry and
-            Information Technology, officially released its third crypto project
-            ranking last week. The new list consists of 31 crypto projects.The
-            center began ranking 28 crypto projects in May. The following month
-            it added 2 more projects: EOS and Nebulas. This month, one more is
-            added, making a total of 31 crypto projects on the list. Gxchain is
-            the latest addition.
+            
+When it comes time to repair or replace your commercial roofing system, you want a roofing contractor who has the knowledge and experience to ensure that your project goes smoothly. Additionally, you want a contractor who offers more than one solution, so that you get the proper roofing system for your building. Crest Exteriors offers multiple options to ensure that you not only get the roof you need, but also the one that meets your budget constraints. We are here to listen to your goals and concerns, and then devise a plan to get you exactly what you need.
+
           </article>
         </div>
         <div className="Home_el-2">
           <article>
             {" "}
-            some cool content goes here blah blah The Center for Information
-            Industry Development (CCID), under China’s Ministry of Industry and
-            Information Technology, officially released its third crypto project
-            ranking last week. The new list consists of 31 crypto projects.The
-            center began ranking 28 crypto projects in May. The following month
-            it added 2 more projects: EOS and Nebulas. This month, one more is
-            added, making a total of 31 crypto projects on the list. Gxchain is
-            the latest addition.
+            
+When your commercial roof needs to be repaired, you need to hire someone who has experience with the complexities of commercial roofing. Not all roofers are the same, and hiring an inexperienced roofer to complete your commercial project is playing with fire…sometimes literally. A commercial roofer needs to be highly trained so that you don’t have to replace your roof again prematurely.
+
           </article>
-          <h1 className="somecoolheader">SOME COOL HEADER</h1>{" "}
+          <h1 className="somecoolheader">Not All Roofers are the Right Roofers</h1>{" "}
         </div>
         <div className="Home_el-3">
-          <h1 className="somecoolheader">SOME COOL HEADER</h1>
+          <h1 className="somecoolheader">Knowing when to Replace Your Roof</h1>
           <article>
             {" "}
-            some cool content goes here blah blah The Center for Information
-            Industry Development (CCID), under China’s Ministry of Industry and
-            Information Technology, officially released its third crypto project
-            ranking last week. The new list consists of 31 crypto projects.The
-            center began ranking 28 crypto projects in May. The following month
-            it added 2 more projects: EOS and Nebulas. This month, one more is
-            added, making a total of 31 crypto projects on the list. Gxchain is
-            the latest addition.
+           A solid roof above your head is pretty crucial if you have become accustomed to having a warm, cozy, and leak free home or business. You need to change your roof if it is over 20 years old, the shingle sare curling, missing, cracked, or covered with moss or algae.
           </article>
         </div>
       </div>
